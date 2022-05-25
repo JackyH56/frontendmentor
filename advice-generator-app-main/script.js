238136a8abc6749a7c5740b0839a03dd706d8ebb
Jacky <jacky_huang56@hotmail.com>
2022-05-25 12:02:29 -0600

    refactor advice-generator-app with BEM

--- advice-generator-app-main/script.js
+++ advice-generator-app-main/script.js
@@ -1,16 +1,16 @@
-const url='https://api.adviceslip.com/advice';
+const url = "https://api.adviceslip.com/advice";
 
 function getQuote() {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", url);
-    xhr.send();
-    
-    xhr.onreadystatechange = (e) => {
-        var json = JSON.parse(xhr.responseText);
-        console.log(json);
-        
-        document.getElementById("advice-number").innerHTML = json.slip.id;
-        document.getElementById("advice-text").innerHTML = '"' + json.slip.advice + '"';
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", url);
+  xhr.send();
 
-    }
+  xhr.onreadystatechange = (e) => {
+    var json = JSON.parse(xhr.responseText);
+    console.log(json);
+
+    document.querySelector(".card__advice-number").innerHTML = json.slip.id;
+    document.querySelector(".card__advice-text").innerHTML =
+      '"' + json.slip.advice + '"';
+  };
 }
