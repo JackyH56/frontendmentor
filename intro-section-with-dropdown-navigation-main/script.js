0b4dff3adadf35e34eb9c6c670fc5ce31851ec3d
Jacky <jacky_huang56@hotmail.com>
2022-05-25 16:30:44 -0600

    refactor desktop intro-section-with-dropdown-navigation-main with BEM

--- intro-section-with-dropdown-navigation-main/script.js
+++ intro-section-with-dropdown-navigation-main/script.js
@@ -2,81 +2,77 @@ let featuresMenuOpen = false;
 let companyMenuOpen = false;
 
 function handleFeaturesDropdown() {
-    let dropdownIcon = document.getElementById("features-dropdown-icon");
-    let dropdownContent = document.getElementById("features-dropdown-content");
+  let dropdownIcon = document.querySelector(".features-dropdown-icon");
+  let dropdownContent = document.querySelector(".features-dropdown-content");
 
-    if (featuresMenuOpen) {
-        dropdownIcon.src="images/icon-arrow-down.svg";
-        featuresMenuOpen = false;
-        dropdownContent.style.display="none";
-    }
-    else {
-        dropdownIcon.src="images/icon-arrow-up.svg";
-        featuresMenuOpen = true;
-        dropdownContent.style.display="flex";
-
-    }
+  if (featuresMenuOpen) {
+    dropdownIcon.src = "images/icon-arrow-down.svg";
+    featuresMenuOpen = false;
+    dropdownContent.style.display = "none";
+  } else {
+    dropdownIcon.src = "images/icon-arrow-up.svg";
+    featuresMenuOpen = true;
+    dropdownContent.style.display = "flex";
+  }
 }
 
 function handleFeaturesDropdownSideNav() {
-    let dropdownIcon = document.getElementById("features-dropdown-icon-side");
-    let dropdownContent = document.getElementById("features-dropdown-content-sidenav");
+  let dropdownIcon = document.querySelector(".features-dropdown-icon-side");
+  let dropdownContent = document.querySelector(
+    ".features-dropdown-content-sidenav"
+  );
 
-    if (featuresMenuOpen) {
-        dropdownIcon.src="images/icon-arrow-down.svg";
-        featuresMenuOpen = false;
-        dropdownContent.style.display="none";
-    }
-    else {
-        dropdownIcon.src="images/icon-arrow-up.svg";
-        featuresMenuOpen = true;
-        dropdownContent.style.display="block";
-
-    }
+  if (featuresMenuOpen) {
+    dropdownIcon.src = ".images/icon-arrow-down.svg";
+    featuresMenuOpen = false;
+    dropdownContent.style.display = "none";
+  } else {
+    dropdownIcon.src = "images/icon-arrow-up.svg";
+    featuresMenuOpen = true;
+    dropdownContent.style.display = "block";
+  }
 }
 
 function handleCompanyDropdown() {
-    let dropdownIcon = document.getElementById("company-dropdown-icon");
-    let dropdownContent = document.getElementById("company-dropdown-content");
-    console.log(dropdownIcon);
-    console.log(dropdownContent);
-    if (companyMenuOpen) {
-        dropdownIcon.src="images/icon-arrow-down.svg";
-        companyMenuOpen = false;
-        dropdownContent.style.display="none";
-    }
-    else {
-        dropdownIcon.src="images/icon-arrow-up.svg";
-        companyMenuOpen = true;
-        dropdownContent.style.display="flex";
-
-    }
+  let dropdownIcon = document.querySelector(".company-dropdown-icon");
+  let dropdownContent = document.querySelector(".company-dropdown-content");
+  console.log(dropdownIcon);
+  console.log(dropdownContent);
+  if (companyMenuOpen) {
+    dropdownIcon.src = "images/icon-arrow-down.svg";
+    companyMenuOpen = false;
+    dropdownContent.style.display = "none";
+  } else {
+    dropdownIcon.src = "images/icon-arrow-up.svg";
+    companyMenuOpen = true;
+    dropdownContent.style.display = "flex";
+  }
 }
 
 function handleCompanyDropdownSideNav() {
-    let dropdownIcon = document.getElementById("company-dropdown-icon-sidenav");
-    let dropdownContent = document.getElementById("company-dropdown-content-sidenav");
-    console.log(dropdownIcon);
-    console.log(dropdownContent);
-    if (companyMenuOpen) {
-        dropdownIcon.src="images/icon-arrow-down.svg";
-        companyMenuOpen = false;
-        dropdownContent.style.display="none";
-    }
-    else {
-        dropdownIcon.src="images/icon-arrow-up.svg";
-        companyMenuOpen = true;
-        dropdownContent.style.display="block";
-
-    }
+  let dropdownIcon = document.querySelector(".company-dropdown-icon-sidenav");
+  let dropdownContent = document.querySelector(
+    "company-dropdown-content-sidenav"
+  );
+  console.log(dropdownIcon);
+  console.log(dropdownContent);
+  if (companyMenuOpen) {
+    dropdownIcon.src = "images/icon-arrow-down.svg";
+    companyMenuOpen = false;
+    dropdownContent.style.display = "none";
+  } else {
+    dropdownIcon.src = "images/icon-arrow-up.svg";
+    companyMenuOpen = true;
+    dropdownContent.style.display = "block";
+  }
 }
 
-  /* Set the width of the side navigation to 250px */
+/* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("sidenav").style.width = "250px";
+  document.querySelector(".sidenav").style.width = "250px";
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
-    document.getElementById("sidenav").style.width = "0";
+  document.querySelector(".sidenav").style.width = "0";
 }
