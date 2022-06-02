54a77a97af78a468f49e60e8a63f6ae54d237c38
Jacky <jacky_huang56@hotmail.com>
2022-06-02 16:33:38 -0600

    sunnyside landing-page with css grid (testimonial section buggy)

--- sunnyside-agency-landing-page-main/script.js
+++ sunnyside-agency-landing-page-main/script.js
@@ -2,12 +2,11 @@ const hamburgerIcon = document.querySelector(".header__hamburger-icon");
 const menu = document.querySelector(".header__menu");
 
 function handleMenu() {
-
-  if(menu.style.display === "block") {
+  if (menu.style.display === "block") {
     menu.style.display = "none";
   } else {
-    menu.style.display ="block";
-  };
+    menu.style.display = "block";
+  }
 }
 
 hamburgerIcon.addEventListener("click", handleMenu);
